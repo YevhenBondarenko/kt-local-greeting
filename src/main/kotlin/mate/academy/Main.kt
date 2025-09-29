c848f47adaad8a4ec577995c292fe54793a29f02
YevhenBondarenko <bondarenkojek@gmail.com>
2025-09-29 23:36:28 +0200

    implemented greetUsers function

--- src/main/kotlin/mate/academy/Main.kt
+++ src/main/kotlin/mate/academy/Main.kt
@@ -1,7 +1,16 @@
 package mate.academy
 
 fun greetUsers() {
-    // implement function
+    var name: String? = null
+    while (readLine().also { name = it }?.isNotBlank() == true) {
+        println("Hello $name")
+    }
+
+}
+
+fun readLine(): String? {
+    println("Please enter a name (or press Enter to quit): ")
+    return kotlin.io.readLine();
 }
 
 fun main() {
